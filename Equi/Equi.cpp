639605159311d8fc85b725be75f92320bca10c8f
William Bernardoni <wrbernardoni@gmail.com>
2025-10-03 13:23:11 -0400

    Reorganizing the tests a bit so they display better in visual studios.

--- Equi/Equi.cpp
+++ Equi/Equi.cpp
@@ -12,7 +12,12 @@ int main(int argc, char* argv[])
 {
     std::cout << "EquiLang Version " << EQUI_VERSION_MAJOR << "."
         << EQUI_VERSION_MINOR << std::endl;
-	Equi::HelloWorld();
+
+    std::cout << "argc: " << argc << std::endl;
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << "[" << i << "]:\t" << argv[i] << std::endl;
+    }
 
 	return 0;
 }
